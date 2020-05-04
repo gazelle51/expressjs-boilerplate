9684c5ea0b559b2fad01b2d6223de61a6da82e97
gazelle51 <sarahgazelle@hotmail.com>
2020-05-04 11:38:11 +1000

    clean up transports

--- src/config/winston.ts
+++ src/config/winston.ts
@@ -2,30 +2,22 @@ import appRoot from "app-root-path";
 import httpContext from "express-http-context";
 import winston from "winston";
 import { Format } from "logform";
-import { LogOptions } from "./models/Winston";
 
-// Formatting of dev console logs
+// Formatting of console logs
 const consoleFormat: Format = winston.format.printf(
   ({ level, message, timestamp }) => {
-    let colourLevel: string;
-
-    // Trace ID
-    const traceID = httpContext.get("traceID");
+    let logLevel: string;
 
     // Colour of log
-    colourLevel = winston.format
+    logLevel = winston.format
       .colorize()
       .colorize(level, `${level.toUpperCase()}`);
 
-    // Formatting of requests vs other app logs
-    if (traceID !== undefined) {
-      return `[traceID: ${traceID}]: ${colourLevel} - ${message}`;
-    } else {
-      return `${colourLevel} - ${message}`;
-    }
+    return `${timestamp} [${logLevel}] ${message}`;
   }
 );
 
+// Formatting of JSON logs
 const jsonFormat: Format = winston.format.printf(
   ({ level, message, timestamp, ...meta }) => {
     // Trace ID
@@ -33,73 +25,35 @@ const jsonFormat: Format = winston.format.printf(
 
     // Formatting of JSON
     return JSON.stringify({
-      traceID: traceID,
-      unixTimestamp: Math.round(new Date(timestamp).getTime() / 1000),
-      level: level,
-      message: message,
+      traceID,
+      timestamp, // Math.round(new Date(timestamp).getTime() / 1000),
+      level,
+      message,
       ...meta,
     });
   }
 );
 
-// Custom settings for each transport
-let options: LogOptions = {
-  fileDebug: {
-    level: "debug",
-    filename: `${appRoot.toString()}/logs/app.log`,
-    handleExceptions: true,
-    format: winston.format.combine(winston.format.timestamp(), jsonFormat),
-    maxsize: 5242880, // 5MB
-    maxFiles: 5,
-  },
-  file: {
-    level: "info",
-    filename: `${appRoot.toString()}/logs/app.log`,
-    handleExceptions: true,
-    format: winston.format.combine(winston.format.timestamp(), jsonFormat),
-    maxsize: 5242880, // 5MB
-    maxFiles: 5,
-  },
-  consoleDebug: {
-    level: "debug",
-    handleExceptions: true,
-    format: winston.format.combine(winston.format.timestamp(), consoleFormat),
-  },
-  consoleDev: {
-    level: "info",
-    handleExceptions: true,
-    format: winston.format.combine(winston.format.timestamp(), consoleFormat),
-  },
-  consoleProd: {
-    level: "info",
-    handleExceptions: true,
-    format: winston.format.combine(winston.format.timestamp(), jsonFormat),
-  },
-};
-
-// Create new Winston Logger for each environment
-let logger: winston.Logger;
-if (process.env.NODE_ENV === "production") {
-  logger = winston.createLogger({
-    transports: [new winston.transports.Console(options.consoleProd)],
-    exitOnError: false, // Do not exit on handled exceptions
-  });
-} else if (process.env.NODE_ENV === "debug") {
-  logger = winston.createLogger({
-    transports: [
-      new winston.transports.File(options.fileDebug),
-      new winston.transports.Console(options.consoleDebug),
-    ],
-    exitOnError: false, // Do not exit on handled exceptions
-  });
-} else {
-  logger = winston.createLogger({
-    transports: [
-      new winston.transports.File(options.file),
-      new winston.transports.Console(options.consoleDev),
-    ],
-    exitOnError: false, // Do not exit on handled exceptions
-  });
-}
+// Logger
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console({
+      level: process.env.LOG_LEVEL,
+      format: winston.format.combine(winston.format.timestamp(), consoleFormat),
+    }),
+    new winston.transports.File({
+      filename: `${appRoot.toString()}/logs/app.log`,
+      level: "info",
+      format: winston.format.combine(winston.format.timestamp(), jsonFormat),
+    }),
+    new winston.transports.File({
+      filename: `${appRoot.toString()}/logs/debug.log`,
+      level: "debug",
+      maxsize: 10485760, // 10MB
+      maxFiles: 5,
+      format: winston.format.combine(winston.format.timestamp(), jsonFormat),
+    }),
+  ],
+});
 
 export default logger;
